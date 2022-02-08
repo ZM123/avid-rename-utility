5fd510aca1d320490559414b7fcfdc7981b111d6
Zal Machado <zalmachado@gmail.com>
2022-02-08 18:30:30 -0500

    add shot names function

--- avid_renamer_utility.py
+++ avid_renamer_utility.py
@@ -9,7 +9,7 @@ def read_ale(filename):
 # Retrieves the list containing the column names from an ALE as a list of lists
 def get_columns(row_list):
     for index, row in enumerate(row_list):
-        if len(row) == 1 and row[0] == 'Column' and index < len(row_list) - 1:
+        if len(row) == 1 and row[0] == 'Column':
             return row_list[index + 1]
 
 # Retrieves the data as a list of lists from an ALE as a list of lists
@@ -22,6 +22,20 @@ def get_data(row_list):
 
             return row_list[index + 1:]
 
+# Get the list of shot names given the data columns and data list
+def get_shot_names(columns_list, data_list):
+    name_index = -1
+
+    for index, heading in enumerate(columns_list):
+        if heading == 'Name':
+            name_index = index
+
+    if name_index == -1:
+        print('No Name column found')
+        return []
+
+    return [x[name_index] for x in data_list]
+
 ################################################################################
 name = sys.argv[1]
 
@@ -31,12 +45,7 @@ columns = get_columns(row_list)
 
 data = get_data(row_list)
 
-# name_index = -1
-#
-# for index, heading in enumerate(column_headings):
-#     if heading == 'Name':
-#         name_index = index
-#
-# if name_index == -1:
-#     print('No Name column found')
-#     sys.exit(1)
+if len(columns) == 0 or len(data) == 0:
+    sys.exit(1)
+
+shot_names = get_shot_names(columns, data)
