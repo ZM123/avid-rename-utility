07228c4cbd917126993cd3027c3b323021e4d142
Zal Machado <zalmachado@gmail.com>
2022-02-08 21:24:14 -0500

    Input search and sub and show preview

--- avid_renamer_utility.py
+++ avid_renamer_utility.py
@@ -4,7 +4,7 @@ import csv
 
 # Parses the tab delimited ALE file into a list of lists
 def read_ale(filename):
-    return list(csv.reader(open(name, 'r'), delimiter='\t'))
+    return list(csv.reader(open(filename, 'r'), delimiter='\t'))
 
 # Retrieves the list containing the column names from an ALE as a list of lists
 def get_columns(row_list):
@@ -37,9 +37,9 @@ def get_shot_names(columns_list, data_list):
     return [x[name_index] for x in data_list]
 
 ################################################################################
-name = sys.argv[1]
+filename = sys.argv[1]
 
-row_list = read_ale(name)
+row_list = read_ale(filename)
 
 columns = get_columns(row_list)
 
@@ -49,3 +49,39 @@ if len(columns) == 0 or len(data) == 0:
     sys.exit(1)
 
 shot_names = get_shot_names(columns, data)
+
+if len(shot_names) == 0:
+    print('No shot names found')
+    sys.exit(1)
+
+print('Found shot names:')
+for name in shot_names:
+    print(name)
+
+search_regex = input('Enter search regex:')
+
+# Add surrounding capture group if none specified
+if re.compile(search_regex).groups < 1:
+    search_regex = f'({search_regex})'
+
+print('Found matches:')
+for name in shot_names:
+    print(re.sub(rf'({search_regex})', r'\033[2;31;43m\1\033[0;0m', name))
+
+sub_regex = input('Enter substitution regex, UP for uppercase, LO for lowercase:')
+
+to_lower = False
+to_upper = False
+if sub_regex == 'UP':
+    to_upper = True
+if sub_regex == 'LO':
+    to_lower = True
+
+print('Substitution preview:')
+for name in shot_names:
+    if to_upper:
+        print(re.sub(rf'{search_regex}', lambda m: '\033[2;36;42m{}\033[0;0m'.format(m.group().upper()), name))
+    elif to_lower:
+        print(re.sub(rf'{search_regex}', lambda m: '\033[2;36;42m{}\033[0;0m'.format(m.group().lower()), name))
+    else:
+        print(re.sub(rf'{search_regex}', rf'{sub_regex}', name))
